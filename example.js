de49f7b7edf04546e4f7944d30f2cfdc2df91639
fmay <fmay@applicationcraft.com>
2014-08-26 10:30:52 +0000

    update

--- example.js
+++ example.js
@@ -5,44 +5,40 @@
 * Time: 10:20 AM
 */
 
-function drawCircle() {
-    
-  // Create a canvas x=50 and y=10, 400 side and 300 high
-  var width=400;
-  var height=300;
-  var circleColor="#fff000";  
-  
-  // Create a drawing area (canvas) at the screen coordinate (100,50)
-  var paper = Raphael(100, 50, width, height);
-  
-  // Draw a rectangle that fills the entire rectangle
-  var rect = paper.rect(0, 0, width, height);
-  
-  // Fill it with a color
-  rect.attr("fill", "#f00");
-  
-  // Creates circle at x = 100, y = 50, with radius=30
-  var circle = paper.circle(100, 50, 30);
 
-    
-  // Sets the fill attribute of the circle
-  circle.attr("fill", circleColor);
+function play() {
 
-  // Sets the stroke (border) attribute of the circle to black
-  circle.attr("stroke", "#000000");    
+  // UNARY OPERATORS
+  document.write("UNARY OPERATORS<br/><br/>");
+
+   
+  var temperature=24;
+  var msg="Message : ";
+
+  document.write("Initial value : temp=" + temperature + "<br/><br/>");  
   
-};
+  temperature += 6; 
+  document.write("Step 1 :   temp=" + temperature + "<br/><br/>"); 
 
-function playSound() {
+  temperature *= 10; 
+  document.write("Step 2 :   temp=" + temperature + "<br/><br/>"); 
   
-  var freq1=480;
-  const duration=1000;
+  temperature /= 5; 
+  document.write("Step 3 :   temp=" + temperature + "<br/><br/>"); 
+
+  temperature++;
+  document.write("Step 4 :   temp=" + temperature + "<br/><br/>"); 
+
+  temperature--;
+  document.write("Step 5 :   temp=" + temperature + "<br/><br/>"); 
+
+	msg += "I've had enough of the temperature examples!"  
+  document.write(msg + "<br/><br/>"); 
   
-  var sine1 = T("sin", {freq:freq1, mul:0.5});
-  var sine2 = T("sin", {freq:660, mul:0.5});
-
-  T("perc", {r:duration}, sine1, sine2).on("ended", function() {
-    this.pause();
-  }).bang().play();  
-
 }
+
+
+
+
+
+
